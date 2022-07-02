7c175c7ffef74117b85cc53918b87b33d09f74ab
fr3ncyx <102382602+fr3ncyx@users.noreply.github.com>
2022-07-02 14:30:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ const Discord = require("discord.js")
 const client = new Discord.Client({intents: 32767})
 
 client.login(process.env.token)
+
+client.on("ready", () => {
+    console.log("Bot online")
+})
